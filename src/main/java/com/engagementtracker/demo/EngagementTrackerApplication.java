a6abb8d381ec5654c723a684a6b1ab0734a76ef6
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-24 10:15:07 +0530

    Initial commit

---
+++ src/main/java/com/engagementtracker/demo/EngagementTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.engagementtracker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngagementTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngagementTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engagementtracker/demo/EngagementTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engagementtracker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngagementTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
